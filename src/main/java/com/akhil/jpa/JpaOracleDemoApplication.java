a6e4b482c12fdfe6144eb5f92cbede9bd0e5b412
akhilrangaraju@gmail.com <Akhil.Rangaraju@landsend.com>
2018-12-26 17:34:38 -0600

    First Commit

---
+++ src/main/java/com/akhil/jpa/JpaOracleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.akhil.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaOracleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaOracleDemoApplication.class, args);
+	}
+
+}
+
